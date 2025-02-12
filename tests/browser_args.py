7ef741a02a4e31b82a2cc4f3b47096fb49fb7652
Stephan Lensky <stephanl.public@gmail.com>
2025-02-11 21:17:23 -0500

    Use env-var to disable sandbox

--- tests/browser_args.py
+++ tests/browser_args.py
@@ -3,6 +3,8 @@ import os
 
 logger = logging.getLogger(__name__)
 
+NO_SANDBOX = os.getenv("ZENDRIVER_TEST_NO_SANDBOX", "false") == "true"
+
 
 def get_browser_args(*, headless: bool) -> list[str]:
     args = []
@@ -10,8 +12,8 @@ def get_browser_args(*, headless: bool) -> list[str]:
     if not headless:
         args.extend(["--disable-features=UseOzonePlatform", "--ozone-platform=x11"])
 
-    if os.geteuid() == 0:
-        logger.warning("Running tests as root! Disabling sandbox")
+    if NO_SANDBOX:
+        logger.warning("Detected ZENDRIVER_TEST_NO_SANDBOX=true! Disabling sandbox")
         args.append("--no-sandbox")
 
     return args
