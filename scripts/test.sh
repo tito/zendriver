b1b1616f8496505bbdaec7b85b33cd5ad485257d
Stephan Lensky <stephanl.public@gmail.com>
2025-02-16 19:40:41 -0500

    Use find_chrome_executable in test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,13 +7,11 @@ if [ "${#command[@]}" -eq 0 ]; then
   command=( "pytest" )
 fi
 
-chrome_cmd=("google-chrome")
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  # macOS
-  chrome_cmd=("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome")
-fi
-chrome_cmd+=("--version")
+chrome_executable=$(uv run python -c "from zendriver.core.config import find_chrome_executable;print(find_chrome_executable())")
+echo "Chrome executable: $chrome_executable"
 
+chrome_cmd=( "$chrome_executable" )
+chrome_cmd+=("--version")
 chrome_version=$("${chrome_cmd[@]}")
 echo "Chrome version: $chrome_version"
 
